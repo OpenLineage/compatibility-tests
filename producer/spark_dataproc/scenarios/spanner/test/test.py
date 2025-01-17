cfe543885251b245350e27dc218c2832dbecf876
tnazarew <tomasz.nazarewicz@getindata.com>
2025-01-17 14:15:50 +0100

    fix
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/spark_dataproc/scenarios/spanner/test/test.py
+++ producer/spark_dataproc/scenarios/spanner/test/test.py
@@ -12,36 +12,13 @@ database_id = "test-database"
 
 # Define Spanner table names
 table_name = "TestTable"
-new_table_name = "target_table"
 
-# Sample data
-data = [
-    (1, "Alice", 10),
-    (2, "Bob", 20),
-    (3, "Charlie", 30),
-    (4, "Alice", 40),
-    (5, "Bob", 50),
-]
-columns = ["Id", "Name", "Value"]
-
-
-def set_options(read_or_write, table):
-    return read_or_write.format('cloud-spanner') \
+if __name__ == "__main__":
+    df = spark.read.format('cloud-spanner') \
         .option("projectId", project_id) \
         .option("instanceId", instance_id) \
         .option("databaseId", database_id) \
-        .option("table", table)
-
-
-if __name__ == "__main__":
-    # Ensure the right table content
-    # df = spark.createDataFrame(data, columns)
-    # set_options(df.write, table_name).mode("overwrite").save()
-
-    # trigger operation to get testable OL events
-
-    df = set_options(spark.read, table_name).load()
-    df.createOrReplaceTempView('testTable')
-    aggregated_df = df.groupBy("Name").max("Timestamp").withColumnRenamed("max(Timestamp)", "maxTS")
+        .option("table", table_name).load()
+    aggregated_df = df.groupBy("Name").max("Value").withColumnRenamed("sum(Timestamp)", "totalValue")
     aggregated_df.show()
 
