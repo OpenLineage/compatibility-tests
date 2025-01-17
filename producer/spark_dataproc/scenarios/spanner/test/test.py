22dfcdc7b6b11c6014e78e440a10d102fb53f2ad
tnazarew <tomasz.nazarewicz@getindata.com>
2025-01-17 14:16:13 +0100

    fix
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/spark_dataproc/scenarios/spanner/test/test.py
+++ producer/spark_dataproc/scenarios/spanner/test/test.py
@@ -11,7 +11,7 @@ instance_id = "spaner-openlineage-testing-instance"
 database_id = "test-database"
 
 # Define Spanner table names
-table_name = "TestTable"
+table_name = "test_table"
 
 if __name__ == "__main__":
     df = spark.read.format('cloud-spanner') \
@@ -19,6 +19,6 @@ if __name__ == "__main__":
         .option("instanceId", instance_id) \
         .option("databaseId", database_id) \
         .option("table", table_name).load()
-    aggregated_df = df.groupBy("Name").max("Value").withColumnRenamed("sum(Timestamp)", "totalValue")
+    aggregated_df = df.groupBy("Name").max("Value").withColumnRenamed("sum(Value)", "totalValue")
     aggregated_df.show()
 
