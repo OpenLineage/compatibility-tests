11aff95ff78593ad52a131ef48f196dfc8675e47
tnazarew <tomasz.nazarewicz@getindata.com>
2025-01-20 09:53:11 +0100

    workflow with spanner producer (#97)
    * spanner scenario
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>
    
    ---------
    
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

---
+++ producer/spark_dataproc/scenarios/spanner/test/test.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+
+# Create Spark session
+spark = SparkSession.builder \
+    .appName("Spark Spanner Example") \
+    .getOrCreate()
+
+# Spanner configuration
+project_id = "gcp-open-lineage-testing"
+instance_id = "spaner-openlineage-testing-instance"
+database_id = "test-database"
+
+# Define Spanner table names
+table_name = "test_table"
+
+if __name__ == "__main__":
+    df = spark.read.format('cloud-spanner') \
+        .option("projectId", project_id) \
+        .option("instanceId", instance_id) \
+        .option("databaseId", database_id) \
+        .option("table", table_name).load()
+    aggregated_df = df.groupBy("Name").sum("Value").withColumnRenamed("sum(Value)", "totalValue")
+    aggregated_df.write.csv("output")
+
