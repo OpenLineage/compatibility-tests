8f15c41cd37aacb12a79c53790e6437753e80786
tnazarew <tomasz.nazarewicz@getindata.com>
2025-03-07 15:41:53 +0100

    test
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- scripts/validate_ol_events.py
+++ scripts/validate_ol_events.py
@@ -40,9 +40,18 @@ class OLSyntaxValidator:
         file_paths = listdir(join(spec_path, tag))
         facet_schemas = [load_json(join(spec_path, tag, path)) for path in file_paths if path.__contains__('Facet.json')]
         spec_schema = next(load_json(join(spec_path, tag, path)) for path in file_paths if path.__contains__('OpenLineage.json'))
-        resolver = RefResolver(base_uri=spec_schema['$id'], referrer=spec_schema)
-        schema_validators = {next(iter(schema['properties'])): Draft202012Validator(schema, resolver=resolver) for schema in
-                             facet_schemas}
+        schema_validators = {}
+        for schema in facet_schemas:
+            name = next(iter(schema['properties']))
+            store = {
+                "https://openlineage.io/spec/2-0-2/OpenLineage.json": spec_schema,
+                schema['$id']: schema,
+            }
+            resolver = RefResolver(base_uri="", referrer=spec_schema, store=store)
+            schema_validators[name] = Draft202012Validator(schema, resolver=resolver)
+
+        # schema_validators = {next(iter(schema['properties'])): Draft202012Validator(schema, resolver=resolver) for schema in
+        #                      facet_schemas}
         schema_validators['core'] = Draft202012Validator(spec_schema)
         return cls(schema_validators)
 
