c9a31dd1b05338750b85cb0957f0bdb9147adb8f
tnazarew <tomasz.nazarewicz@getindata.com>
2025-03-27 18:25:29 +0100

    temporary removal of bt jar
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/spark_dataproc/runner/get_openlineage_jar.sh
+++ producer/spark_dataproc/runner/get_openlineage_jar.sh
@@ -5,15 +5,28 @@
 set -euxo pipefail
 
 readonly VM_SPARK_JARS_DIR=/usr/lib/spark/jars
-
-readonly SPARK_BQ_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BQ_CONNECTOR_URL || echo "gs://open-lineage-e2e/jars/spark-3.5-bigquery-0.0.1-SNAPSHOT.jar")
-readonly OPENLINEAGE_SPARK_URL=$(/usr/share/google/get_metadata_value attributes/OPENLINEAGE_SPARK_URL || echo "gs://open-lineage-e2e/jars/openlineage-spark_2.12-1.29.0-SNAPSHOT.jar")
-readonly SPARK_SPANNER_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_SPANNER_CONNECTOR_URL || echo "gs://open-lineage-e2e/jars/spark-3.1-spanner-1.1.0.jar")
+readonly SPARK_BQ_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BQ_CONNECTOR_URL || echo "")
+readonly OPENLINEAGE_SPARK_URL=$(/usr/share/google/get_metadata_value attributes/OPENLINEAGE_SPARK_URL || echo "")
+readonly SPARK_SPANNER_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_SPANNER_CONNECTOR_URL || echo "")
 readonly SPARK_BIGTABLE_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BIGTABLE_CONNECTOR_URL || echo "")
-readonly POSTGRES_URL=$(/usr/share/google/get_metadata_value attributes/POSTGRES_URL || echo "gs://open-lineage-e2e/jars/postgresql-42.5.6.jar")
 
-gsutil cp -P "${SPARK_BQ_CONNECTOR_URL}" "${VM_SPARK_JARS_DIR}/"
-gsutil cp -P "${OPENLINEAGE_SPARK_URL}" "${VM_SPARK_JARS_DIR}/"
-gsutil cp -P "${SPARK_SPANNER_CONNECTOR_URL}" "${VM_SPARK_JARS_DIR}/"
-gsutil cp -P "${POSTGRES_URL}" "${VM_SPARK_JARS_DIR}/"
-[[ -n "${SPARK_BIGTABLE_CONNECTOR_URL}" ]] && gsutil cp -P "${SPARK_BIGTABLE_CONNECTOR_URL}" "${VM_SPARK_JARS_DIR}/"
+
+if [[ -n "${OPENLINEAGE_SPARK_URL}" ]]; then
+    bq_url="${SPARK_BQ_CONNECTOR_URL}"
+    ol_url="${OPENLINEAGE_SPARK_URL}"
+    spanner_url="${SPARK_SPANNER_CONNECTOR_URL}"
+    bigtable_url="${SPARK_BIGTABLE_CONNECTOR_URL}"
+else
+    bq_url="gs://open-lineage-e2e/jars/spark-3.5-bigquery-0.0.1-SNAPSHOT.jar"
+    ol_url="gs://open-lineage-e2e/jars/openlineage-spark_2.12-1.29.0-SNAPSHOT.jar"
+    spanner_url="gs://open-lineage-e2e/jars/spark-3.1-spanner-1.1.0.jar"
+    bigtable_url="gs://open-lineage-e2e/jars/spark-bigtable_2.12-0.4.0.jar"
+fi
+
+postgresql_url="gs://open-lineage-e2e/jars/postgresql-42.5.6.jar"
+
+gsutil cp -P "${bq_url}" "${VM_SPARK_JARS_DIR}/"
+gsutil cp -P "${ol_url}" "${VM_SPARK_JARS_DIR}/"
+gsutil cp -P "${spanner_url}" "${VM_SPARK_JARS_DIR}/"
+gsutil cp -P "${postgresql_url}" "${VM_SPARK_JARS_DIR}/"
+gsutil cp -P "${bigtable_url}" "${VM_SPARK_JARS_DIR}/"
