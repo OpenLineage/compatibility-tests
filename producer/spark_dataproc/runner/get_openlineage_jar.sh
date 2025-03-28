85d5746227e5d9d7ee630c9274a493efdb4109b0
tnazarew <tomasz.nazarewicz@getindata.com>
2025-03-28 17:38:10 +0100

    disable bt and ignore jar
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/spark_dataproc/runner/get_openlineage_jar.sh
+++ producer/spark_dataproc/runner/get_openlineage_jar.sh
@@ -8,7 +8,7 @@ readonly VM_SPARK_JARS_DIR=/usr/lib/spark/jars
 readonly SPARK_BQ_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BQ_CONNECTOR_URL || echo "")
 readonly OPENLINEAGE_SPARK_URL=$(/usr/share/google/get_metadata_value attributes/OPENLINEAGE_SPARK_URL || echo "")
 readonly SPARK_SPANNER_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_SPANNER_CONNECTOR_URL || echo "")
-readonly SPARK_BIGTABLE_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BIGTABLE_CONNECTOR_URL || echo "")
+#readonly SPARK_BIGTABLE_CONNECTOR_URL=$(/usr/share/google/get_metadata_value attributes/SPARK_BIGTABLE_CONNECTOR_URL || echo "")
 
 
 if [[ -n "${OPENLINEAGE_SPARK_URL}" ]]; then
@@ -20,7 +20,7 @@ else
     bq_url="gs://open-lineage-e2e/jars/spark-3.5-bigquery-0.0.1-SNAPSHOT.jar"
     ol_url="gs://open-lineage-e2e/jars/openlineage-spark_2.12-1.29.0-SNAPSHOT.jar"
     spanner_url="gs://open-lineage-e2e/jars/spark-3.1-spanner-1.1.0.jar"
-    bigtable_url="gs://open-lineage-e2e/jars/spark-bigtable_2.12-0.4.0.jar"
+#    bigtable_url="gs://open-lineage-e2e/jars/spark-bigtable_2.12-0.4.0.jar"
 fi
 
 postgresql_url="gs://open-lineage-e2e/jars/postgresql-42.5.6.jar"
@@ -29,4 +29,4 @@ gsutil cp -P "${bq_url}" "${VM_SPARK_JARS_DIR}/"
 gsutil cp -P "${ol_url}" "${VM_SPARK_JARS_DIR}/"
 gsutil cp -P "${spanner_url}" "${VM_SPARK_JARS_DIR}/"
 gsutil cp -P "${postgresql_url}" "${VM_SPARK_JARS_DIR}/"
-gsutil cp -P "${bigtable_url}" "${VM_SPARK_JARS_DIR}/"
+#gsutil cp -P "${bigtable_url}" "${VM_SPARK_JARS_DIR}/"
