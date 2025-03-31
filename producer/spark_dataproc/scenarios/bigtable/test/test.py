ec80c755762deea5373af6fee1d28f8b0d0c0d05
tnazarew <tomasz.nazarewicz@getindata.com>
2025-03-31 13:12:05 +0200

    add drop table if exists to bt tests
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/spark_dataproc/scenarios/bigtable/test/test.py
+++ producer/spark_dataproc/scenarios/bigtable/test/test.py
@@ -15,6 +15,11 @@ def generate_test_row(number, start_range):
         doubleCol=float(number / 3.14)
     )
 
+def drop_if_exists(instance, table_name):
+    table = instance.table(table_id=table_name)
+    if table.exists():
+        print(f"Deleting existing table: {table_name}")
+        table.delete()
 
 def generate_table_id(test_name):
     return f"cbt-{test_name}-{uuid.uuid4().hex[:20]}"
@@ -92,6 +97,12 @@ raw_basic_catalog = ("""
 project_id = "gcp-open-lineage-testing"
 instance_id = "openlineage-test"
 
+client = bigtable.Client(project=project_id, admin=True)
+instance = client.instance(instance_id)
+
+drop_if_exists(instance, input_table)
+drop_if_exists(instance, output_table)
+
 write_dataframe_to_bigtable(test_df, raw_basic_catalog, project_id, instance_id, True)
 
 read_df = read_dataframe_from_bigtable(spark, raw_basic_catalog, project_id, instance_id)
@@ -113,11 +124,7 @@ write_dataframe_to_bigtable(output_df, output_catalog, project_id, instance_id, 
 
 spark.stop()
 
-#Cleanup after the run
-
-client = bigtable.Client(project=project_id, admin=True)
-
-instance = client.instance(instance_id)
+# Cleanup after the run
 
 bt_table1 = instance.table(table_id=input_table)
 bt_table2 = instance.table(table_id=output_table)
