3784bf9e403718b93e24615832f6a2937c5cdceb
tnazarew <tomasz.nazarewicz@getindata.com>
2025-07-14 00:33:10 +0200

    fix structure
    Signed-off-by: tnazarew <tomasz.nazarewicz@getindata.com>

--- producer/hive_dataproc/runner/dataproc_workflow.py
+++ producer/hive_dataproc/runner/dataproc_workflow.py
@@ -223,7 +223,7 @@ async def create_cluster(
         "project_id": project_id,
         "cluster_name": cluster_name,
         "config": {
-            "staging_bucket": "open-lineage-e2e",
+            "config_bucket": "open-lineage-e2e",
             "gce_cluster_config": {
                 "network_uri": "default",
                 "internal_ip_only": False,
